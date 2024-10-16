81567169e2a7b18360743335900c13276b2f207b
Remi26d <remidelarue11290@gmail.com>
2024-10-16 09:52:33 +0200

    modif IJ

--- src/main/java/org/github/izunsky/Main.java
+++ src/main/java/org/github/izunsky/Main.java
@@ -4,8 +4,9 @@ public class Main {
     // commentaire
     // 2° commentaire
     // 3° commentaire
+
+    //commentaire sur intelligent idea
     public static void main(String[] args) {
         System.out.println("Hello world!");
     }
 }
-// test pouche
