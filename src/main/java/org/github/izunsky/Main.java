6ba211e1c1965cee5c432e1fcb298286ba680052
Adrien Izunsky <adrienizunsky@gmail.com>
2024-10-16 09:15:58 +0200

    2° commit 3° commit

--- src/main/java/org/github/izunsky/Main.java
+++ src/main/java/org/github/izunsky/Main.java
@@ -2,6 +2,8 @@ package org.github.izunsky;
 
 public class Main {
     // commentaire
+    // 2° commentaite
+    // 3° commentaire
     public static void main(String[] args) {
         System.out.println("Hello world!");
     }
