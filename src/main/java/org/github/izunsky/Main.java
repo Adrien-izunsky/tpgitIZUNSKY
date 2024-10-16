36960458302ac4b445cbb58038e3bade36873b54
Remi26d <remidelarue11290@gmail.com>
2024-10-16 09:34:35 +0200

    test push

--- src/main/java/org/github/izunsky/Main.java
+++ src/main/java/org/github/izunsky/Main.java
@@ -2,9 +2,10 @@ package org.github.izunsky;
 
 public class Main {
     // commentaire
-    // 2° commentaite
+    // 2° commentaire
     // 3° commentaire
     public static void main(String[] args) {
         System.out.println("Hello world!");
     }
 }
+//test pouche
